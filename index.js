b1eba123af95c41b74267d6f9c13d1366f46e8e5
Lindsay Carbonell <lindsaycarbonell@gmail.com>
2017-10-03 17:56:25 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+var express = require('express');
+var app = express();
+var ejs = require('ejs');
+
+app.use(express.static(__dirname + '/public'))
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.set('port', process.env.PORT || 5000)
+
+app.listen(app.get('port'), function(){
+  console.log('Node app is running on port', app.get('port'));
+});
+
+app.get('/', function(req, res){
+  res.render('index');
+})
