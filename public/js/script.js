bd3e39efcccc1a4db6f5cb4d0a03be26031542d0
Lindsay Carbonell <lindsaycarbonell@gmail.com>
2017-10-04 17:59:24 -0400

    ready

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,7 @@
 $(window).bind("load", function() {
 
     "use strict";
-    
+
     $(".spn_hol").fadeOut(1000);
 });
 
@@ -11,11 +11,11 @@ $(window).bind("load", function() {
 $(document).ready(function() {
 
     "use strict";
-    
+
     $(window).scroll(function() {
 
         "use strict";
-        
+
         if ($(window).scrollTop() > 80) {
             $(".navbar").css({
                 'margin-top': '0px',
@@ -61,11 +61,11 @@ $(document).ready(function() {
 $(document).ready(function() {
 
     "use strict";
-    
+
     $(".navbar-nav li a").click(function() {
 
         "use strict";
-        
+
         $(".navbar-nav li a").parent().removeClass("active");
         $(this).parent().addClass("active");
     });
@@ -78,15 +78,15 @@ $(document).ready(function() {
 $(document).ready(function() {
 
     "use strict";
-    
+
     $(window).scroll(function() {
 
         "use strict";
-        
+
         $(".page").each(function() {
 
             "use strict";
-            
+
             var bb = $(this).attr("id");
             var hei = $(this).outerHeight();
             var grttop = $(this).offset().top - 70;
@@ -105,7 +105,7 @@ $(document).ready(function() {
 
 
 $(function() {
-	
+
 	"use strict";
 
   $('a[href*=#]:not([href=#])').click(function() {
@@ -128,11 +128,11 @@ $(function() {
 $(document).ready(function() {
 
     "use strict";
-    
+
     setInterval(function() {
 
         "use strict";
-        
+
         var widnowHeight = $(window).height();
         var containerHeight = $(".home-container").height();
         var padTop = widnowHeight - containerHeight;
@@ -149,7 +149,7 @@ $(document).ready(function() {
 $(document).ready(function() {
 
     "use strict";
-    
+
     $(window).bind('load', function() {
         "use strict";
         parallaxInit();
@@ -167,32 +167,32 @@ $(document).ready(function() {
 
 
 //OWL CAROSEL
-$(document).ready(function() {
+// $(document).ready(function() {
+//
+//     "use strict";
+//
+//     $("#owl-demo").owlCarousel({
+//         autoPlay: 3000,
+//         items: 4, //10 items above 1000px browser width
+//         itemsDesktop: [1370, 3], //5 items between 1000px and 901px
+//         itemsDesktopSmall: [900, 2], // betweem 900px and 601px
+//         itemsTablet: [600, 1], //2 items between 600 and 0
+//     });
+// });
 
-    "use strict";
-    
-    $("#owl-demo").owlCarousel({
-        autoPlay: 3000,
-        items: 4, //10 items above 1000px browser width
-        itemsDesktop: [1370, 3], //5 items between 1000px and 901px
-        itemsDesktopSmall: [900, 2], // betweem 900px and 601px
-        itemsTablet: [600, 1], //2 items between 600 and 0
-    });
-});
 
 
-    
  //PRETTYPHOTO
 
-$(document).ready(function() {
-
-    "use strict";
-
-    $("a[rel^='prettyPhoto']").prettyPhoto({
-        show_title: false,
-        /* true/false */
-    });
-});
+// $(document).ready(function() {
+//
+//     "use strict";
+//
+//     $("a[rel^='prettyPhoto']").prettyPhoto({
+//         show_title: false,
+//         /* true/false */
+//     });
+// });
 
 
 
@@ -200,116 +200,95 @@ $(document).ready(function() {
 $(document).ready(function() {
 
     "use strict";
- 
+
     new WOW().init();
 });
 
 
 
 //RESPONSIVE VIDEO
-$(document).ready(function() {
-
-    "use strict";
-    
-    // Basic FitVids Test
-    $(".video").fitVids();
-});
+// $(document).ready(function() {
+//
+//     "use strict";
+//
+//     // Basic FitVids Test
+//     $(".video").fitVids();
+// });
 
 
 
 //CONTACT FORM VALIDATION
-$(document).ready(function() {
-
-    "use strict";
-    
-    $(".form_submit").click(function() {
-
-        "use strict";
-        
-        var name = $("#name").val();
-        var emaild = $("#email").val();
-        var subject = $("#subject").val();
-        var message = $("#message").val();
-        var testEmail = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i;
-        if (!name) {
-            $(".form_error .name_error").addClass("show").removeClass("hide");
-            return false;
-        } else {
-            $(".form_error .name_error").addClass("hide").removeClass("show");
-        }
-        if (!emaild) {
-            $(".form_error .email_error").addClass("show").removeClass("hide");
-            return false;
-        } else {
-            $(".form_error .email_error").addClass("hide").removeClass("show");
-            if (testEmail.test(emaild)) {
-                $(".form_error .email_val_error").addClass("hide").removeClass("show");
-            } else {
-                $(".form_error .email_val_error").addClass("show").removeClass("hide");
-                return false;
-            }
-        }
-        if (!message) {
-            $(".form_error .message_error").addClass("show").removeClass("hide");
-            return false;
-        } else {
-            $(".form_error .message_error").addClass("hide").removeClass("show");
-        }
-        if (name && emaild && message) {
-            $.ajax({
-                url: 'contact.php',
-                data: {
-                    name: name,
-                    emaild: emaild,
-                    subject: subject,
-                    message: message
-                },
-                type: 'POST',
-                success: function(data) {
-                    $(".Sucess").show();
-                    $(".Sucess").fadeIn(2000);
-                    $(".Sucess").html("<i class='fa fa-check'></i> Dear <b>" + name + "</b> Thank you for your inquiry we will respond to you as soon as possible!");
-                    $("#Name").val("");
-                    $("#Email").val("");
-                    $("#Subject").val("");
-                    $("#Message").val("");
-                    $(".form_error .name_error, .form_error .email_error, .form_error .email_val_error, .form_error .message_error").addClass("hide").removeClass("show");
-                    $("#name").val("");
-                    $("#email").val("");
-                    $("#subject").val("");
-                    $("#message").val("");
-                }
-            });
-        }
-        return false;
-    });
-});
+// $(document).ready(function() {
+//
+//     "use strict";
+//
+//     $(".form_submit").click(function() {
+//
+//         debugger
+//
+//         "use strict";
+//
+//         var name = $("#name").val();
+//         var emaild = $("#email").val();
+//         var subject = $("#subject").val();
+//         var message = $("#message").val();
+//         var testEmail = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i;
+//         if (!name) {
+//             $(".form_error .name_error").addClass("show").removeClass("hide");
+//             return false;
+//         } else {
+//             $(".form_error .name_error").addClass("hide").removeClass("show");
+//         }
+//         if (!emaild) {
+//             $(".form_error .email_error").addClass("show").removeClass("hide");
+//             return false;
+//         } else {
+//             $(".form_error .email_error").addClass("hide").removeClass("show");
+//             if (testEmail.test(emaild)) {
+//                 $(".form_error .email_val_error").addClass("hide").removeClass("show");
+//             } else {
+//                 $(".form_error .email_val_error").addClass("show").removeClass("hide");
+//                 return false;
+//             }
+//         }
+//         if (!message) {
+//             $(".form_error .message_error").addClass("show").removeClass("hide");
+//             return false;
+//         } else {
+//             $(".form_error .message_error").addClass("hide").removeClass("show");
+//         }
+//         if (name && emaild && message) {
+//             debugger
+//         }
+//         return false;
+//     });
+// });
 
 
- 
-/// SMOOTH SCROLL           
 
-$(document).ready(function() {
+/// SMOOTH SCROLL
 
-    "use strict";
-    
-    var scrollAnimationTime = 1200,
-        scrollAnimation = 'easeInOutExpo';
-    $('a.scrollto').bind('click.smoothscroll', function(event) {
-        event.preventDefault();
-        var target = this.hash;
-        $('html, body').stop().animate({
-            'scrollTop': $(target).offset().top
-        }, scrollAnimationTime, scrollAnimation, function() {
-            window.location.hash = target;
-        });
-    });
-    //COUNTER
-    $('.counter_num').counterUp({
-        delay: 10,
-        time: 2000
-    });
-});
+// $(document).ready(function() {
+//
+//     "use strict";
+//
+//     var scrollAnimationTime = 1200,
+//         scrollAnimation = 'easeInOutExpo';
+//     $('a.scrollto').bind('click.smoothscroll', function(event) {
+//         event.preventDefault();
+//         var target = this.hash;
+//         $('html, body').stop().animate({
+//             'scrollTop': $(target).offset().top
+//         }, scrollAnimationTime, scrollAnimation, function() {
+//             window.location.hash = target;
+//         });
+//     });
+//     //COUNTER
+//     $('.counter_num').counterUp({
+//         delay: 10,
+//         time: 2000
+//     });
+// });
 
 
 
@@ -317,13 +296,13 @@ $(document).ready(function() {
 
 
 //VIDEO BACKGROUND
-$(document).ready(function() {
-  var videobackground = new $.backgroundVideo($('body'), {
-    "align": "centerXY",
-    "width": 1280,
-    "height": 720,
-    "path": "media/",
-    "filename": "cloud",
-    "types": ["mp4","ogg","webm"]
-  });
-});
+// $(document).ready(function() {
+//   var videobackground = new $.backgroundVideo($('body'), {
+//     "align": "centerXY",
+//     "width": 1280,
+//     "height": 720,
+//     "path": "media/",
+//     "filename": "cloud",
+//     "types": ["mp4","ogg","webm"]
+//   });
+// });
