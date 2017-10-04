bd83b95ba10e913520a8d0eb376e3a470f306eaa
Lindsay Carbonell <lindsaycarbonell@gmail.com>
2017-10-04 18:38:01 -0400

    idk anymore

--- public/js/script.js
+++ public/js/script.js
@@ -125,23 +125,23 @@ $(function() {
 
 
 // FIX HOME SCREEN HEIGHT
-$(document).ready(function() {
-
-    "use strict";
-
-    setInterval(function() {
-
-        "use strict";
-
-        var widnowHeight = $(window).height();
-        var containerHeight = $(".home-container").height();
-        var padTop = widnowHeight - containerHeight;
-        $(".home-container").css({
-            'padding-top': Math.round(padTop / 2) + 'px',
-            'padding-bottom': Math.round(padTop / 2) + 'px'
-        });
-    }, 10)
-});
+// $(document).ready(function() {
+//
+//     "use strict";
+//
+//     setInterval(function() {
+//
+//         "use strict";
+//
+//         var widnowHeight = $(window).height();
+//         var containerHeight = $(".home-container").height();
+//         var padTop = widnowHeight - containerHeight;
+//         $(".home-container").css({
+//             'padding-top': Math.round(padTop / 2) + 'px',
+//             'padding-bottom': Math.round(padTop / 2) + 'px'
+//         });
+//     }, 10)
+// });
 
 
 
