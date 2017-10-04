1c23555ee8417e0bb7630bf34ec663a1774d5432
Lindsay Carbonell <lindsaycarbonell@gmail.com>
2017-10-04 08:11:16 -0400

    change colors and widths

--- index.js
+++ index.js
@@ -7,12 +7,18 @@ app.use(express.static(__dirname + '/public'))
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.set('port', process.env.PORT || 5000)
+app.set('port', process.env.PORT || 5000);
 
 app.listen(app.get('port'), function(){
   console.log('Node app is running on port', app.get('port'));
 });
 
+var creds = require('<client key JSON file>');
+
+var GoogleSpreadsheet = require("google-sheets-node-api");
+
+var mySheet = new GoogleSpreadsheet('<spreadsheet ID>');
+
 app.get('/', function(req, res){
-  res.render('index');
-})
+  res.render('index', {'mySheet': mySheet, 'creds':creds});
+});
