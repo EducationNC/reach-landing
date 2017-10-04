af2689292fbbab12d42ad5d6fc0f7d0aa9d52062
Lindsay Carbonell <lindsaycarbonell@gmail.com>
2017-10-04 08:15:35 -0400

    bug fix attempt 2

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ app.listen(app.get('port'), function(){
   console.log('Node app is running on port', app.get('port'));
 });
 
-var creds = require('<client key JSON file>');
-
-var GoogleSpreadsheet = require("google-sheets-node-api");
-
-var mySheet = new GoogleSpreadsheet('<spreadsheet ID>');
+// var creds = require('<client key JSON file>');
+//
+// var GoogleSpreadsheet = require("google-sheets-node-api");
+//
+// var mySheet = new GoogleSpreadsheet('<spreadsheet ID>');
 
 app.get('/', function(req, res){
   res.render('index')
